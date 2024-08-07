4b8680f5e5f4ad363d927c344edb8ead63e0e602
ABDELLAH MOUTAWAKKIL <abdo.moutawakkil2002@gmail.com>
2024-08-07 20:18:11 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { NavigationMenuDemo } from './components/Heading'
+
+function App() {
+
+
+  return (
+    <>
+      <NavigationMenuDemo />
+    </>
+  )
+}
+
+export default App
